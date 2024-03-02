42e3ec09a4fe3149c93c0582c3038c56c6fd6697
r8mishra <ravi.2.mishra@nokia.com>
2024-03-02 12:16:16 +0530

    new

--- JAVA_DSA/src/A01_HelloWorld.java
+++ JAVA_DSA/src/A01_HelloWorld.java
@@ -1,6 +1,10 @@
 public class A01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
     
