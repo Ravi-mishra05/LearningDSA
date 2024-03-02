cd2e31921a610ef7883de7c0c57e11b42d9d7381
r8mishra <ravi.2.mishra@nokia.com>
2024-03-02 15:33:21 +0530

    new changes

--- JAVA_DSA/src/L11_Recursion/C1_Print1toN.java
+++ JAVA_DSA/src/L11_Recursion/C1_Print1toN.java
@@ -1,18 +1,18 @@
 package L11_Recursion;
 
-public class L2_Print1toN {
+public class C1_Print1toN {
     public static void main(String[] args) {
         printnum(1);
     }
 
     static void printnum(int n) {
         // int start = 0;
-        if (n == 5) {  // base conditon
+        if (n == 5) { // base conditon
             System.out.print(5);
             return;
         }
 
         System.out.print(n + " "); // body
-        printnum(n + 1);  //recursive call
+        printnum(n + 1); // recursive call
     }
 }
