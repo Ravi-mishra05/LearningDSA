b2a643d817050fb82656b9aff9ad7492698a4c65
r8mishra <ravi.2.mishra@nokia.com>
2024-04-21 01:15:53 +0530

    changes

---
+++ JAVA_DSA/src/Questions/S1_LoveBabbar/T1_Arrays/Q10_MinimumNumberOfJumps.java
@@ -0,0 +1,13 @@
+package Questions.S1_LoveBabbar.T1_Arrays;
+
+public class Q10_MinimumNumberOfJumps {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 3, 2, 6, 7};
+        System.out.println(minJumps(arr));
+    }
+    public static int minJumps(int[] arr){
+        int jumps = 0;
+
+        return jumps;
+    }
+}
--- JAVA_DSA/src/Questions/S1_LoveBabbar/T1_Arrays/Q9_MinimizeTheHeightsII.java
+++ JAVA_DSA/src/Questions/S1_LoveBabbar/T1_Arrays/Q9_MinimizeTheHeightsII.java
@@ -1,5 +1,7 @@
 package Questions.S1_LoveBabbar.T1_Arrays;
 
+import java.util.Arrays;
+
 public class Q9_MinimizeTheHeightsII {
     public static void main(String[] args) {
         int k = 2, n = 4;
@@ -8,6 +10,17 @@ public class Q9_MinimizeTheHeightsII {
         System.out.println(ans);
     }
     public static int getMinDiff(int[] arr, int n, int k) {
-        
+        if (n == 1) return 0;
+        Arrays.sort(arr);
+        int ans = arr[n - 1] - arr[0];
+
+        for (int i = 0;i < n;i++) {
+            if (arr[i] < k) continue;
+            int curr_min = Math.min(arr[0] + k, arr[i] - k);
+            int curr_max = Math.max(arr[n - 1] - k, arr[i - 1] + k);
+            if (curr_max < curr_min) continue;
+            ans = Math.min(ans, curr_max - curr_min);
+        }
+        return ans;
     }
 }
