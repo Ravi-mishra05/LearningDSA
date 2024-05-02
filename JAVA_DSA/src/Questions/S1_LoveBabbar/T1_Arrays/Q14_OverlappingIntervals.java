e0f3d878a4e5d5e0b897b47405010d342ffd2b9b
r8mishra <ravi.2.mishra@nokia.com>
2024-05-02 11:40:37 +0530

    changes

--- JAVA_DSA/src/Questions/S1_LoveBabbar/T1_Arrays/Q14_OverlappingIntervals.java
+++ JAVA_DSA/src/Questions/S1_LoveBabbar/T1_Arrays/Q14_OverlappingIntervals.java
@@ -1,11 +1,14 @@
 package Questions.S1_LoveBabbar.T1_Arrays;
 
+import java.util.Arrays;
+
 public class Q14_OverlappingIntervals {
     public static void main(String[] args) {
-
+        int[][] arr = {{6,8},{1,9},{2,4},{4,7}};
+        System.out.println(Arrays.deepToString(overlappedInterval(arr)));
     }
     public static int[][] overlappedInterval(int[][] Intervals) {
-        // Code here
 
+        return new int[][]{{0, 0}, {0, 0}};
     }
 }
